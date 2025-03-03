5f6cbf00d3b201cb785f71c0add46077293600eb
klozz <carlosj@klozz.dev>
2025-03-03 05:58:58 +0000

    adapt to xperience
    Signed-off-by: klozz <carlosj@klozz.dev>

--- hidl/touch/TouchscreenGesture.cpp
+++ hidl/touch/TouchscreenGesture.cpp
@@ -28,7 +28,7 @@ using ::android::base::Trim;
 namespace {}  // anonymous namespace
 
 namespace vendor {
-namespace lineage {
+namespace xperience {
 namespace touch {
 namespace V1_0 {
 namespace implementation {
@@ -79,5 +79,5 @@ Return<bool> TouchscreenGesture::setGestureEnabled(const Gesture& gesture, bool 
 }  // namespace implementation
 }  // namespace V1_0
 }  // namespace touch
-}  // namespace lineage
+}  // namespace xperience
 }  // namespace vendor
--- hidl/touch/service.cpp
+++ hidl/touch/service.cpp
@@ -25,10 +25,10 @@ using android::sp;
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-using vendor::lineage::touch::V1_0::IHighTouchPollingRate;
-using vendor::lineage::touch::V1_0::ITouchscreenGesture;
-using vendor::lineage::touch::V1_0::implementation::HighTouchPollingRate;
-using vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
+using vendor::xperience::touch::V1_0::IHighTouchPollingRate;
+using vendor::xperience::touch::V1_0::ITouchscreenGesture;
+using vendor::xperience::touch::V1_0::implementation::HighTouchPollingRate;
+using vendor::xperience::touch::V1_0::implementation::TouchscreenGesture;
 
 int main() {
     sp<IHighTouchPollingRate> highToushPollingRateService = new HighTouchPollingRate();
