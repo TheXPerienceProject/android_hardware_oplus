1e02cc22fc237e97b413b1c82260c5909a7547e0
Sean Du <do4suki@gmail.com>
2025-06-26 22:53:08 +0800

    oplus-fwk: Enable IR feature
    Change-Id: Ia2b4a3edd728885374c2bafe5fa99d029e6abb7e
    Signed-off-by: Sean Du <do4suki@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- oplus-fwk/src/com/oplus/content/OplusFeatureConfigManager.java
+++ oplus-fwk/src/com/oplus/content/OplusFeatureConfigManager.java
@@ -24,6 +24,9 @@ public class OplusFeatureConfigManager {
             }
             return true;
         }
+        if ("oplus.hardware.type.rear_IR".equals(name)) {
+            return true;
+        }
         return false;
     }
 
